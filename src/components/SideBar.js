a22a4921de0eedfb1a98a75b0d87309d588442f9
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-10-22 15:40:41 +0100

    Add nav buttons on the side

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
+import HomeSelected from "../assets/HomeOrange.png"
+import HomeUnselected from "../assets/HomeBlue.png"
+import ScheduleSelected from "../assets/ScheduleSelected.png"
+import ScheduleUnselected from "../assets/ScheduleBlue.png"
 
 const SideBar = () => {
+
+    const [ homeSelected, setHomeSelected ] = useState(true)
+
+    const homeButton = homeSelected ? HomeSelected : HomeUnselected
+    const scheduleButton = homeSelected ? ScheduleUnselected : ScheduleSelected
+
     return (
         <div className="sidenav">
-            <nav>
+            <nav className="sidenav-buttons">
                 <ul>
-                    <li className="heading sidenav"><Link to="/">Home</Link></li>
-                    <li className="heading sidenav"><Link to="/schedule">Schedule</Link></li>
+                    <li onClick={() => {setHomeSelected(true)}}>
+                        <Link to="/">
+                            <img className="nav-image" src={homeButton} alt="Home" width="100%"/>
+                        </Link>
+                    </li>
+                    <li onClick={() => {setHomeSelected(false)}}>
+                        <Link to="/schedule">
+                            <img className="nav-image" src={scheduleButton} alt="Schedule" width="100%"/>
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </div>
