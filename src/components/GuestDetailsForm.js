4b2408fd67585fcc0f7e7c8d2057c02ddd86b897
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-10-25 09:37:54 +0000

    Add guest details form component

---
+++ src/components/GuestDetailsForm.js
@@ -0,0 +1,59 @@
+import React from "react"
+import FormLabel from "./FormLabel"
+import { useForm } from "react-hook-form"
+import LookingGlass from "../assets/LookingGlass.png"
+
+const GuestDetailsForm = ({guests, users, displayDropdown, setDisplayDropdown, addGuests, handleSearchInput }) => {
+
+    const { register, watch } = useForm()
+
+    const addedGuests = guests.map(guestId => {
+        const guest = users.find(user => user.id === guestId)
+        return (
+            <li className="heading secondary" key={guest.email}>{guest.name}</li>
+        )
+    })
+
+    const handleGuestsSelected = () => {
+        addGuests(watch("guests"))
+    }
+
+    let guestOptions = []
+
+    if (displayDropdown) {
+        guestOptions = users.map(user => {
+            return (
+                <div key={user.id}>
+                    <input onChange={handleGuestsSelected} checked={guests.includes(user.id) ? true : false} type="checkbox" className="dropdown-content" id={user.id} name="guests" ref={register} value={user.id}/>
+                    <label className="heading secondary dropdown-label" htmlFor={user.id}>{user.name}</label>
+                </div>
+            )
+        })
+    }
+
+    const onSearchInput = () => {
+        // handleSearchInput(watch("search-contacts"))
+    }
+    
+    return (
+        <div className="form-details small-flex-item">
+            <form>
+                <FormLabel htmlFor="guests" label="Add Guests" />
+                <div className="search">
+                    <input onChange={onSearchInput} onClick={() => setDisplayDropdown(!displayDropdown)} className="input-field" type="text" name="search-contacts" placeholder="Search" ref={register} />
+                    <img className="looking-glass" id="search-icon-form" src={LookingGlass} alt="Search"/>
+                </div>
+                <div className="guest-dropdown" >
+                    {guestOptions}
+                </div>
+                <div>
+                    <ul className="guest-list" id={displayDropdown ? "hidden" : ""}>
+                        {addedGuests}
+                    </ul>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default GuestDetailsForm
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react"
-import { useForm } from "react-hook-form"
 import PageHeader from "./PageHeader"
 import MeetingDetailsForm from "./MeetingDetailsForm"
-import FormLabel from "./FormLabel"
-import LookingGlass from "../assets/LookingGlass.png"
+import GuestDetailsForm from "./GuestDetailsForm"
 
 const Schedule = ({ users, handleNewMeeting, searchContacts }) => {
 
-    const { register, handleSubmit, watch, errors } = useForm()
     const [ guests, setGuests ] = useState([])
     const [ message, setMessage ] = useState("")
     const [ displayDropdown, setDisplayDropdown ] = useState(false)
@@ -39,35 +36,14 @@ const Schedule = ({ users, handleNewMeeting, searchContacts }) => {
         return new Date(endTime)
     }
 
-    const addGuests = () => {
-        const addedGuests = watch("guests").map(id => parseInt(id))
+    const addGuests = (guestsSelected) => {
+        const addedGuests = guestsSelected.map(id => parseInt(id))
         setGuests(addedGuests)
     }
 
     const removeMessage = () => {
         setMessage("")
     }
-
-    let guestOptions = []
-
-    if (displayDropdown) {
-        guestOptions = users.map(user => {
-            return (
-                <div key={user.id}>
-                    <input onChange={addGuests} checked={guests.includes(user.id) ? true : false} type="checkbox" className="dropdown-content" id={user.id} name="guests" ref={register} value={user.id}/>
-                    <label className="heading secondary dropdown-label" htmlFor={user.id}>{user.name}</label>
-                </div>
-            )
-        })
-    }
-
-    const addedGuests = guests.map(guestId => {
-        const cross = '\u2716'
-        const guest = users.find(user => user.id === guestId)
-        return (
-            <li className="heading secondary" key={guest.email}>{guest.name}</li>
-        )
-    })
 
     const handleSearchInput = (searchInput) => {
         //Out of scope for exercise but should handle search contacts with searchContacts() here
@@ -78,23 +54,7 @@ const Schedule = ({ users, handleNewMeeting, searchContacts }) => {
             <PageHeader title={header}/>
             <div className="content-container flex-content" onClick={removeMessage}>
                 <MeetingDetailsForm message={message} saveMeeting={saveMeeting}/>
-                <div className="form-details small-flex-item">
-                    <form action="">
-                        <FormLabel htmlFor="guests" label="Add Guests" />
-                        <div className="search">
-                            <input onChange={handleSearchInput} onClick={() => setDisplayDropdown(!displayDropdown)} className="input-field" type="text" name="search" placeholder="Search" ref={register} />
-                            <img className="looking-glass" id="search-icon-form" src={LookingGlass} alt="Search"/>
-                        </div>
-                        <div className="guest-dropdown" >
-                            {guestOptions}
-                        </div>
-                        <div>
-                            <ul className="guest-list" id={displayDropdown ? "hidden" : ""}>
-                                {addedGuests}
-                            </ul>
-                        </div>
-                    </form>
-                </div>
+                <GuestDetailsForm guests={guests} users={users} displayDropdown={displayDropdown} setDisplayDropdown={setDisplayDropdown} addGuests={addGuests}/>
             </div>
         </div>
     )
